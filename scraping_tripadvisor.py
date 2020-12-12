d802ca7d6346de06ead6511faa4c8de9a0a537cc
Alex-bensimon <64471842+Alex-bensimon@users.noreply.github.com>
2020-12-12 16:48:35 +0100

    scraping ok mais pb ID à régler

--- scraping_tripadvisor.py
+++ scraping_tripadvisor.py
@@ -20,7 +20,7 @@ hôtels.
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
+from random import randint
 import time
 from time import sleep
 from selenium.webdriver.common.keys import Keys
@@ -67,7 +67,7 @@ for i in range(nb_page):
     home_soup = BeautifulSoup(homepage.text, 'lxml')
     
     for a in home_soup.find_all('a',  class_='property_title prominent'):
-        
+        # On parcourt chaque hotel et on récupère le lien
         hotels_name.append(a.text.replace('\n',"").strip())
         #print(hotels_name)
         link = a['href'].replace('\n',"")
@@ -77,28 +77,21 @@ for i in range(nb_page):
         
         hotel_content = requests.get(full_link)
         hotel_soup = BeautifulSoup(hotel_content.text, 'lxml')
+        
+        # On prend ses caractéristiques
         hotels_rate.append(hotel_soup.find('span', class_="_3cjYfwwQ").text)
         hotels_location.append(hotel_soup.find('span', class_="_3ErVArsu jke2_wbp").text)
         hotels_id.append(h_id)
-        
-        # Cette partie est inutile, finalement on ne prend pas la description
-        #driver = get_driver()
-        #driver.get(full_link)
-        #sleep(5)
-        
-        #description_plus = driver.find_element_by_css_selector("div[class='_36B4Vw6t']")
-        #description_plus.click()
-        
+       
         
         # Partie où on s'occupe des avis
         
         # Là, c'est les 5 div des commentaires
         reviews = hotel_soup.find_all('div', class_='_2wrUUKlw _3hFEdNs8')
         for review in reviews:
-            # PROBLEME : ça prend toujours le 1er commentaire 5 fois 
-            reviews_name.append(hotel_soup.find('a', class_="ui_header_link _1r_My98y").text)
-            
-            rating = hotel_soup.find('span', class_="ui_bubble_rating")
+            # On prend les caractéristiques d el'avis
+            reviews_name.append(review.find('a', class_="ui_header_link _1r_My98y").text)
+            rating = review.find('span', class_="ui_bubble_rating")
             char_to_delete = 'abcdefghijklmnopqrstuvwxyz<>/="_'
             
             for char in char_to_delete:
@@ -107,13 +100,67 @@ for i in range(nb_page):
             new_rate = create_rating(rating)
                 
             reviews_rate.append(new_rate)
-            reviews_title.append(hotel_soup.find('a', class_="ocfR3SKN").text)
+            reviews_title.append(review.find('a', class_="ocfR3SKN").text)
             reviews_id.append(c_id)
             id_hotel.append(id_h)
                 
             c_id = c_id + 1
             print("comment nb : "+str(c_id))
-        
+            
+            # On teste pour savoir si l'user a mis + d'un commentaire
+            print(review.find('span', class_='_1fk70GUn').text)
+            if len(review.find('span', class_='_1fk70GUn').text) <= 4:
+                if int(review.find('span', class_='_1fk70GUn').text.strip()) > 1:
+                    # Si oui, on récupère le lien de son profil
+                    user = review.find('a', class_='ui_header_link _1r_My98y')
+                    user_link = user['href']
+                    full_user_link = "https://www.tripadvisor.fr" + user_link
+                    user_content = requests.get(full_user_link)
+                    user_soup = BeautifulSoup(user_content.text, 'lxml')
+                    
+                    # On parcourt tous les commentaires laissés par l'user
+                    user_reviews = user_soup.find_all('div', class_='nMewIgXP ui_card section')
+                    for user_review in user_reviews:
+                        div = user_review.find('div', class_='_2X5tM2jP _2RdXRsdL _1gafur1D')
+                        href_user = div.find('a')
+                        href = href_user['href']
+                        
+                        # On chercher à savoir si le commentaire est sur un hôtel ou restau
+                        test = href[1:6]
+                        
+                        if test == "Hotel":
+                            
+                            # On récupère les caractéristiques du commentaire
+                            reviews_name.append(user_review.find('a', class_="ui_link _1r_My98y").text)
+                            rating = user_review.find('span', class_="ui_bubble_rating")
+                            char_to_delete = 'abcdefghijklmnopqrstuvwxyz<>/="_'
+                            
+                            for char in char_to_delete:
+                                rating = str(rating).replace(char,"").strip()
+                                
+                            new_rate = create_rating(rating)
+                            reviews_rate.append(new_rate)
+                            if user_review.find('div', class_="_3IEJ3tAK _2K4zZcBv") != None:
+                                reviews_title.append(user_review.find('div', class_="_3IEJ3tAK _2K4zZcBv").text)
+                            else:
+                                reviews_title.append(None)
+                            reviews_id.append(c_id)
+                            
+                            new_hotel_link = "https://www.tripadvisor.fr" + href
+                            new_hotel = requests.get(new_hotel_link)
+                            new_hotel_soup = BeautifulSoup(new_hotel.text, 'lxml')
+                            
+                            hotels_name.append(new_hotel_soup.find('h1', class_="_1mTlpMC3").text.replace('\n',"").strip())
+                            hotels_link.append(new_hotel_link)
+                            hotels_rate.append(new_hotel_soup.find('span', class_="_3cjYfwwQ").text)
+                            hotels_location.append(new_hotel_soup.find('span', class_="_3ErVArsu jke2_wbp").text)
+                            new_hotel_id = randint(10000, 100000)
+                            hotels_id.append(new_hotel_id)
+                            id_hotel.append(new_hotel_id)
+                            
+                            print("comment nb : "+str(c_id))
+                            c_id = c_id + 1
+                    
         h_id = h_id + 1
         id_h = h_id
         print("Hotel nb : "+str(h_id))
