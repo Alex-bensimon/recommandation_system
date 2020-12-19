c2073ae8c5882c954a74cdd518ec47c03d4e6308
Alex-bensimon <64471842+Alex-bensimon@users.noreply.github.com>
2020-12-19 18:43:05 +0100

    TFIDF etc

--- recommadation.py
+++ recommadation.py
@@ -7,6 +7,12 @@ Created on Thu Dec 17 13:36:59 2020
 
 
 import pandas as pd 
+import nltk 
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 hotels = pd.read_csv('hotels.csv')
 reviews = pd.read_csv('reviews.csv', index_col = 'h_id')
@@ -42,12 +48,36 @@ for review in hotels_id_list:
         liste = rev.tolist()
         # On convertit la liste en string car on veut juste tous les mots
         liste2 = ' '.join([str(elem) for elem in liste]) 
+    
+    txt = liste2
+    txt.lower().split()
+    txt = txt.lower()
+    sentence_list = sent_tokenize(txt)    
+    word_list = word_tokenize(txt)
+
+    liste_ponct = [",", ".", "!", "?", ";"]
+    word_to_del = liste_ponct + stopwords.words('french')
+    
+    word_list = [word for word in word_list if word not in word_to_del]
+
+    port_stemmer = PorterStemmer()
+    port_stemmer_word_list = []
+    
+    for port_stem in word_list:
+        port_stem_word = port_stemmer.stem(port_stem)
+        port_stemmer_word_list.append(port_stem_word)
         
+    new_list =  ' '.join([str(elem) for elem in port_stemmer_word_list]) 
     # On ajoute nos variables aux tableaux
-    all_reviews.append(liste2)
+    all_reviews.append(new_list)
     all_h_id.append(review)
 
 # On créé le df à partir des 2 tableaux
 new_df = pd.DataFrame({'Hotel id':all_h_id,'reviews': all_reviews})
 
 print(new_df)
+
+reviews = new_df['reviews']
+tfidfs = TfidfVectorizer()
+tf_matrice = tfidfs.fit_transform(reviews)
+
